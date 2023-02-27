a5d14edded8306bbb482a69cd93e2e0038ad22e2
DT3MP01 <albertoalvaza@gmail.com>
2023-02-27 16:21:45 +0100

    Revert "Update test.js"
    This reverts commit 6b8df56a82103bd223d7efd6a2b4b435a6a8bc35.

--- js/test.js
+++ js/test.js
@@ -31,7 +31,7 @@ function init()
 {
   renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.setClearColor( new THREE.Color(0xF5441E) );
+  renderer.setClearColor( new THREE.Color(0xFFFFFF) );
   document.getElementById('container').appendChild( renderer.domElement );
 
   scene = new THREE.Scene();
